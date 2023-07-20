dad3ff77dc19edc7df8ac5e1726717dd87fc9dc0
junghoonkim <crowruya@colosseum.kr>
2023-07-20 16:39:40 +0900

    leetcode q9

--- AlgorithmJava/src/main/java/study/algorithm/leetCode/Q9_PalindromeNumber/Q9_PalindromeNumber.java
+++ AlgorithmJava/src/main/java/study/algorithm/leetCode/Q9_PalindromeNumber/Q9_PalindromeNumber.java
@@ -11,13 +11,8 @@ public class Q9_PalindromeNumber {
 
     public static boolean isPalindrome(int x) {
 
-        System.out.println("--- x : " + x );
-
         String word = Integer.toString(x);
         StringBuffer sb = new StringBuffer(word).reverse();
-
-        System.out.println("--- word : " + word );
-        System.out.println("--- sb.toString() : " + sb.toString());
 
         if(word.equals(sb.toString())){
             return true;
