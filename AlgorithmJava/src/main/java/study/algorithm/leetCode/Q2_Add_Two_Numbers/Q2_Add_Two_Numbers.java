ab2ada6b552c089d6cfb19cf98e24655dbc3a9b3
junghoonkim <crowruya@colosseum.kr>
2023-08-10 16:48:14 +0900

    q2

---
+++ AlgorithmJava/src/main/java/study/algorithm/leetCode/Q2_Add_Two_Numbers/Q2_Add_Two_Numbers.java
@@ -0,0 +1,31 @@
+package study.algorithm.leetCode.Q2_Add_Two_Numbers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q2_Add_Two_Numbers {
+
+    public static void main(String[] args){
+
+        ListNode l1 = new ListNode();
+        ListNode l2 = new ListNode();
+
+        addTwoNumbers(l1, l2);
+
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+
+ }
+
