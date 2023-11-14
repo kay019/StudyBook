d2e97c3eccff9098d883d2d2d28725bcf6e1d16a
kay019 <crowruya@gmail.com>
2023-11-14 10:58:47 +0900

    Q2235 Add Two Integers

---
+++ AlgorithmJava/src/main/java/study/algorithm/leetCode/Q2235_AddTwoIntegers/Q2235_AddTwoIntegers.java
@@ -0,0 +1,17 @@
+package study.algorithm.leetCode.Q2235_AddTwoIntegers;
+
+public class Q2235_AddTwoIntegers {
+
+    public static void main(String[] args){
+
+        sum(1, 2);
+
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+
+ }
+
