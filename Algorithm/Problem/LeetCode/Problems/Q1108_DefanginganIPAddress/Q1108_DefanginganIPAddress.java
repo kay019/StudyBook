ebf6718ff84e1e2b576099f9f69eea82ebc2c67a
junghoonkim <crowruya@colosseum.kr>
2023-12-19 12:40:25 +0900

    Q1108 Defangingan IP Address

--- Algorithm/Problem/LeetCode/Problems/Q1108_DefanginganIPAddress/Q1108_DefanginganIPAddress.java
+++ Algorithm/Problem/LeetCode/Problems/Q1108_DefanginganIPAddress/Q1108_DefanginganIPAddress.java
@@ -1,30 +1,13 @@
 package study.algorithm.leetCode.Problems.Q2236_RootEqualsSumofChildren;
 
-public class Q2236_RootEqualsSumofChildren {
-
-    public String defangIPaddr(String address) {
-        char[] string = address.toCharArray();
-        String result = "";
-        for(int i = 0; i <= string.length - 1; i++){
-            if(Character.compare('.', string[i]) == 0){
-                result += "[.]";
-            } else {
-                result += string[i];
-            }
+public class Q1920_BuildArrayfromPermutation{
+    public int[] buildArray(int[] nums) {
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            result[i] =  nums[nums[i]];
         }
+
         return result;
-    }
-
-    public String defangIPaddr(String address) {
-        return address.replace("." , "[.]");
-    }
-
-    public String defangIPaddr(String address) {
-        StringBuilder str = new StringBuilder();
-        for (char c : address.toCharArray()){
-            str.append((c == '.')? "[.]" : c );
-        }
-        return str.toString();
     }
 
  }
